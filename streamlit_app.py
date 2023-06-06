c4565a83b642c3a76a92beb0197c0f0b1e8ad572
svaggar <33664225+svaggar@users.noreply.github.com>
2023-06-06 13:13:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@
 import streamlit
 streamlit.title('My parents first healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.body('Omega 3, Blueburey & oatmeal \
+streamlit.body('Omega 3, Blueburey & oatmeal \n
                Kale , spinach & Rocket Smoothie')
